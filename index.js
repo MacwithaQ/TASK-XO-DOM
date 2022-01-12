3aefd03fb18b18f732cb5bedb2c32730f0fd8468
MacwithaQ <maqallaf@gmail.com>
2022-01-12 20:29:13 +0300

    alternating x and o

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function winningAlert(winner) {
 
 // SAMPLE CODE: This code fills the 1st and 9th button with X and O initially
 // ❗️ Delete this code once you are done testing
-fillButton(1, "X");
-fillButton(9, "O");
+// fillButton(1, "X");
+// fillButton(9, "O");
 
 /**
  *
@@ -22,9 +22,36 @@ fillButton(9, "O");
  * Add your code here, since this is going to be your main function
  * That interacts with the UI
  */
+
+let playerOne = "X";
+let playerTwo = "O";
+let turn = 0;
+let lastClicked;
+let onePos = [];
+let twoPos = [];
+let index1 = [];
+let index2 = [];
+let winningPositions = [[1,4,7],[2,5,8],[3,6,9],[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7]];
+
 function clickButton(index) {
   console.log(`Button number ${index} is clicked`);
   // Your main code here.
+  // player = checkPlayer();
+  // fillButton(index, player);
+  if (turn % 2 === 0) {
+    fillButton(index, playerOne);
+    lastClicked = playerOne;
+    index1.push(index);
+    console.log(index1);
+    checkWinner();
+  } else {
+    fillButton(index, playerTwo);
+    lastClicked = playerTwo;
+    index2.push(index)
+    console.log(index2);
+    checkWinner();
+  }
+  turn += 1;
 }
 
 /**
@@ -44,5 +71,16 @@ function checkPlayer() {
  * who is winning and returns the winner
  */
 // function checkWinner
+function checkWinner() {
+
+  if (lastClicked=playerOne) {
+    index1.forEach()
+    
+  } else {
+    
+  }
+
+
+}
 
 // function restartGame
